6b227413c9e3f10180340f414bff3545c74ffa15
Ivan Puchkov <rake.eclipse@gmail.com>
2014-05-23 10:29:46 +0700

    fix facebook fetcher

--- lib/fetchers/facebook_fetcher.rb
+++ lib/fetchers/facebook_fetcher.rb
@@ -1,27 +1,49 @@
+require 'progress_bar'
+
 class FacebookFetcher
-  include Sidekiq::Worker
-
-  def initialize
-    @facebook = Koala::Facebook::GraphAPI.new(Settings['facebook.access_token'], Settings['facebook.access_token_secret'])
-  end
-
   def update_newest_stat
-    Entry.newest.each do |entry|
+    entries = Entry.newest
+    pb = ProgressBar.new(entries.count)
+
+    entries.each do |entry|
       update_entry_stat(entry)
+      pb.increment!
     end
   end
 
   def update_older_stat
-    Entry.order(:facebook_updated_at).limit(500).each do |entry|
+    entries = Entry.order(:facebook_updated_at).limit(500)
+    pb = ProgressBar.new(entries.count)
+
+    entries.each do |entry|
       update_entry_stat(entry)
+      pb.increment!
     end
   end
 
+  def update_all
+    entries = Entry.all
+    pb = ProgressBar.new(entries.count)
+
+    entries.each do |entry|
+      update_entry_stat(entry)
+      pb.increment!
+    end
+  end
+
+  private
+
   def update_entry_stat(entry)
-    facebook_search = @facebook.get_objects(entry.url)[entry.url]
-    entry.facebook_shares_count = facebook_search['shares']
-    entry.facebook_comments_count = facebook_search['comments']
+    entry.facebook_shares_count = get_stats(entry.url).first['share_count']
+    entry.facebook_comments_count = get_stats(entry.url).first['comment_count']
+    entry.facebook_likes_count = get_stats(entry.url).first['like_count']
     entry.facebook_updated_at = Time.zone.now
     entry.save
+  end
+
+  def get_stats(url)
+    fql = "SELECT url, share_count, like_count, comment_count, total_count FROM link_stat WHERE url='#{url}'"
+    fetch_url = "https://api.facebook.com/method/fql.query?format=json&query=#{fql}"
+    JSON.parse(open(URI.encode(fetch_url)).read)
   end
 end
